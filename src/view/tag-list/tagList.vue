63f201fa8716a74f1471ac93ce820d31133cb772
huqinqin <896268808@qq.com>
2018-09-11 14:19:39 +0800

    标签管理列表

--- src/view/tag-list/tagList.vue
+++ src/view/tag-list/tagList.vue
@@ -5,7 +5,19 @@
         <li v-for="(item) in tagList" :key="item.value" :value="item.value">
           <Tag v-if="show" closable @on-close="handleClose" color="primary">{{item.label}}</Tag>
         </li>
+        <div class="newtags"><i-button @click="newTag" type="primary">新增标签</i-button></div>
       </ul>
+      <Modal
+        v-model="newTagList"
+        title="新增标签"
+        :loading="loading"
+        @on-ok="asyncOK">
+        <Form ref="formValidate" :model="formValidate" :rules="ruleValidate" :label-width="80">
+          <FormItem label="标签名称" prop="tagsName">
+            <Input v-model="formValidate.tagsName"  placeholder="Enter something..."></Input>
+          </FormItem>
+        </Form>
+      </Modal>
     </div>
 </template>
 
@@ -15,6 +27,8 @@ export default {
   data () {
     return {
       show: true,
+      newTagList: false,
+      loading: true,
       tagList: [{
         value: '情感',
         label: '情感'
@@ -24,12 +38,39 @@ export default {
       }, {
         value: '技术',
         label: '技术'
-      }]
+      }],
+      formValidate: {
+        tagsName: ''
+      },
+      ruleValidate: {
+        tagsName: [
+          {required: true, message: 'The name cannot be empty', trigger: 'blur'}
+        ]
+      }
     }
   },
   methods: {
     handleClose () {
       this.show = false
+    },
+    asyncOK () {
+      this.$refs.formValidate.validate((valid) => {
+        if (valid) {
+          this.loading = false
+          this.newTagList = false
+          this.$Message.success('Success!')
+        } else {
+          this.loading = true
+          setTimeout(() => {
+            this.loading = false
+            this.newTagList = true
+          }, 1000)
+          this.$Message.error('Fail!')
+        }
+      })
+    },
+    newTag () {
+      this.newTagList = true
     }
   }
 }
@@ -41,6 +82,7 @@ export default {
       margin-bottom: 10px;
     }
     ul{
+      position: relative;
       padding: 10px;
       box-sizing: border-box;
       width: 100%;
@@ -49,6 +91,11 @@ export default {
       box-shadow: 0 0 7px 1px #bbb;
       display: flex;
       li{
+      }
+      .newtags{
+        position: absolute;
+        bottom: 10px;
+        right: 40%;
       }
     }
   }
